b586cc30c08dae498ac704442642688bc708a312
johnTjohansen <jtjohansen@ca.rr.com>
2017-03-20 11:11:57 -0700

    save before reboot

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -40,11 +40,44 @@
 
   });
 
-  // When the database changes...
-  database.ref().on("value", function(snapshot) {
-
+  // Initial and when the database changes...
+  database.ref().on("child_added", function(snapshot) {
+  	console.log(snapshot.val());
   	var sv = snapshot.val();
+  	var name = snapshot.val().name;
+  	var dest = snapshot.val().dest;
+  	var freq = snapshot.val().freq;
+  	var firstTime = snapshot.val().tTime;
 
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+    console.log(firstTimeConverted);
+
+    // Current Time
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("HH:mm"));
+
+    // Difference between the times
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+
+    // Time apart (remainder)
+    var tRemainder = diffTime % freq;
+    console.log(tRemainder);
+
+    // Minute Until Train
+    var tMinutesTillTrain = freq - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+    // Next Train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
+  	$("#trainInfo").append("<tr class='table-row'><td> " + name +
+        " </td><td> " + dest + " </td><td> " + freq +    
+        " </td><td> " + nextTrain + 
+        " </td><td> " + tMinutesTillTrain + "</td></tr>");
+        
   });
 
   // Functions
